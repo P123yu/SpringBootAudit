22ee6be59873f8ac8002b79609e63e27eb0f9f3f
Piyush Kumar Singh <96623633+P123yu@users.noreply.github.com>
2025-08-01 16:55:58 +0530

    Update BaseClass.java

--- src/main/java/com/springboot_practice/model/BaseClass.java
+++ src/main/java/com/springboot_practice/model/BaseClass.java
@@ -1,3 +1,42 @@
+
+
+package com.springboot_practice.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+//import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseClass {
+
+    @CreatedDate //@CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+}
+
+
+
+// or =======================================================================================================================
+
+
+
+
 package com.springboot_practice.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
